93fdd42a052313a41a704cc23b5e87005a94ebe5
fujisii <51264835+fujisii@users.noreply.github.com>
2021-01-16 22:12:55 +0900

    done #13

---
+++ Assets/Scripts/PaddleScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Update()は画面が描画されるたびに実行されるため、
+        // 実行するマシンのスペックによって実行回数が異なってくる、という問題がある
+        //
+        // それを解決する方法として Time.deltaTime を使うと良い
+        // -> 遅いマシンでは大きな値、速いマシンでは小さな値をかけてくれる
+        //   -> 結果としてどのマシンでも同じ速度になる
+        transform.position += new Vector3(Input.GetAxis("Horizontal") * Time.deltaTime, 0f, 0f);
+        // Debug.Log(transform.position.x);
+    }
+}

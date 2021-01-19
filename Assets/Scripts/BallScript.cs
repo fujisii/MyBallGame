9d9dfe0acf7d33df41d79592f54bdbaf48954a88
fujisii <51264835+fujisii@users.noreply.github.com>
2021-01-19 23:20:38 +0900

    done #21

--- Assets/Scripts/BallScript.cs
+++ Assets/Scripts/BallScript.cs
@@ -16,14 +16,25 @@ public class BallScript : MonoBehaviour
     void Update()
     {
         transform.position += new Vector3(0f, 0f, -1 * speed * Time.deltaTime);
+
+        // ボールを逃したらゲームオーバー
+        if (transform.position.z < -13.0f)
+        {
+            Debug.Log("Game Over");
+            // ゲーム内の時間を止める
+            Time.timeScale = 0;
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Paddle"))
         {
+            // パドルにボールが当たったらボールを削除する
             Destroy(gameObject);
+            // パドルにボールが当たったらパドルの長さを短くしていく
             collision.gameObject.transform.localScale -= new Vector3(Random.Range(0.2f, 1.0f), 0f, 0f);
+            // パドルの長さが指定値より短くなったら固定する
             if (collision.gameObject.transform.localScale.x < 1.0f)
             {
                 collision.gameObject.transform.localScale = new Vector3(1, 1.0f, 1.0f);
